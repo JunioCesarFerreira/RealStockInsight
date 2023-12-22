a5583ad2243f3a0549096e65609c13df50bbce9a
Junio Cesar Ferreira <jcf_ssp@hotmail.com>
2023-12-22 11:15:11 -0300

    review - Removido Casssandra DB

--- project/cassandra-db/run-cql-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CQL_PATH="./init-script.cql"
-
-CASSANDRA_CONTAINER="cassandra-db"
-
-docker cp ${CQL_PATH} ${CASSANDRA_CONTAINER}:/init-script.cql
-
-docker exec -it ${CASSANDRA_CONTAINER} cqlsh -f /init-script.cql
